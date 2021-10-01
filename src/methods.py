cf1e54b6d025b4e51583f2c8250dde84b64d4863
Vemund <vemund@live.com>
2021-10-01 20:35:57 +0200

    Updates

--- src/methods.py
+++ src/methods.py
@@ -1,5 +1,7 @@
 import torch
+import numpy as np
 import matplotlib.pyplot as plt
+import copy
 
 from ratsimulator import trajectory_generator
 import torch.utils.data as tdata
@@ -33,11 +35,12 @@ def generic_train_loop(model, trainloader, optimizer, criterion, nepochs):
 
 
 class Dataset(torch.utils.data.Dataset):
-    def __init__(self, brain, batch_size=64, nsteps=100, *args, **kwargs):
+    def __init__(self, brain, batch_size=64, nsteps=100, return_cartesian=False, *args, **kwargs):
         self.tg = trajectory_generator(*args, **kwargs)
         self.brain = brain
         self.batch_size = batch_size
         self.nsteps = nsteps
+        self.return_cartesian = return_cartesian
 
     def __len__(self):
         """
@@ -58,19 +61,21 @@ class Dataset(torch.utils.data.Dataset):
         print(f"{worker_id=}, {index=}")
         """
         pos, vel = next(self.tg)[:2]
-        pos = self.brain.softmax_response(pos)
-        init_pos, labels = pos[0], pos[1:]
-        vel = vel[1:]  # first velocity is a dummy velocity
         # OBS! data is not set to device here, since allocating gpu-memory in parallel is
         # non-trivial. Data should be put on correct device in training loop instead.
-        init_pos = torch.tensor(init_pos, dtype=torch.float32)
+        pos = torch.tensor(pos, dtype=torch.float32)
         vel = torch.tensor(vel, dtype=torch.float32)
-        labels = torch.tensor(labels, dtype=torch.float32)
+        pc_pos = self.brain.softmax_response(pos)
+        init_pos, labels = pc_pos[0], pc_pos[1:]
+        vel = vel[1:]  # first velocity is a dummy velocity
 
+        if self.return_cartesian:
+            # for prediction phase (NOT training)
+            return [[vel, init_pos], labels, pos]
         return [[vel, init_pos], labels]
 
 
-def grid_rate_map(
+def rate_map(
     model,
     environment,
     dataset,
@@ -81,6 +86,7 @@ def grid_rate_map(
     *args,
     **kwargs,
 ):
+    bckp_tg = dataset.tg 
     board = environment.get_board(res)
     num_response_maps = (idxs.stop - idxs.start) // idxs.step
     response_maps = np.zeros((num_response_maps, *res))
@@ -109,7 +115,7 @@ def grid_rate_map(
                 pos = np.cumsum(
                     np.concatenate([p0, inputs[0].detach().numpy()]), axis=0
                 )
-
+                # assign activity to space (from environment coordinates to pixel-coordinates) 
                 pos_idxs = np.around((res-1) * pos / np.array(environment.boxsize)).astype(int)
                 model_cell_response = model(inputs).detach().cpu().numpy()
                 response_maps[
@@ -117,6 +123,7 @@ def grid_rate_map(
                 ] += model_cell_response[0, :, idxs].T
                 count_maps[:, pos_idxs[:-1, 0], pos_idxs[:-1, 1]] += 1
 
+    dataset.tg = bckp_tg
     rate_maps = response_maps / count_maps
     return board, rate_maps, response_maps, count_maps
 
