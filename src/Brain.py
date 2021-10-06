b3f687d6578aae2c8c8372e78591036b29dad1d1
Vemund <vemund@live.com>
2021-10-06 16:51:48 +0200

    makes softmax_response parameters class parameters

--- src/Brain.py
+++ src/Brain.py
@@ -1,7 +1,7 @@
 import torch
 
 class Brain:
-    def __init__(self, env, npcs, sigma=0.12, dtype=torch.float32):
+    def __init__(self, env, npcs, sigma=0.12, DoG=False, surround_scale=2, p=2.0, compute_mode="use_mm_for_euclid_dist_if_necessary", dtype=torch.float32):
         """
         Args:
                 env: ABCEnvironment class instance
@@ -13,8 +13,12 @@ class Brain:
         self.pcs = torch.tensor(self.pcs, dtype=dtype)
         self.npcs = npcs
         self.sigma = sigma
+        self.DoG = DoG
+        self.surround_scale = surround_scale
+        self.p = p
+        self.compute_mode = compute_mode
 
-    def softmax_response(self, pos, DoG=False, surround_scale=2, p=2.0, compute_mode="use_mm_for_euclid_dist_if_necessary"):
+    def softmax_response(self, pos):
         """Place cell response as modelled by Sorscher"""
         if len(pos.shape) == 1:
             pos = pos[None]
@@ -26,15 +30,15 @@ class Brain:
             pos = pos.reshape(pos.shape[0] * pos.shape[1], pos.shape[-1])
 
         # distance to place cell center
-        dists = torch.cdist(pos, self.pcs, p=p, compute_mode=compute_mode)
+        dists = torch.cdist(pos, self.pcs, p=self.p, compute_mode=self.compute_mode)
         activity = torch.nn.functional.softmax(-dists / (2 * self.sigma ** 2), dim=-1)
 
-        if DoG:
-            activity -= torch.nn.functional.softmax(-dists / (2 * surround_scale * self.sigma ** 2), dim=-1)
+        if self.DoG:
+            activity -= torch.nn.functional.softmax(-dists / (2 * self.surround_scale * self.sigma ** 2), dim=-1)
 
             # after DoG, activity is not a probability dist anymore
             # shift and rescale s.t it becomes a prob dist again.
-            activity -= torch.min(activity, dim=-1, keepdim=True)
+            activity -= torch.min(activity, dim=-1, keepdim=True).values # returns idxs and values
             activity /= torch.sum(activity, dim=-1, keepdim=True)
 
         return activity.reshape(out_shape)
