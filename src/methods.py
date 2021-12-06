0ab8a1321092bfe6f352a6b15e9904751845e8d8
Vemund Schøyen <vemund@live.com>
2021-12-06 16:06:13 +0000

    logical clean-up

--- src/methods.py
+++ src/methods.py
@@ -5,137 +5,70 @@ from scipy import signal
 import scipy
 import pickle
 
-from ratsimulator import trajectory_generator
+from ratsimulator import Agent
 
 
 class Dataset(torch.utils.data.Dataset):
     def __init__(
-        self,
-        environment,
-        place_cells,
-        batch_size=64,
-        seq_len=20,
-        nsteps=100,
-        return_cartesian=False,
-        **kwargs,
+        self, environment, place_cells, seq_len=20, dataset_size=int(1e6), **kwargs,
     ):
-        self.environment = environment
+        self.agent = Agent(environment, **kwargs)
         self.place_cells = place_cells
-        self.batch_size = batch_size
         self.seq_len = seq_len
-        self.nsteps = nsteps
-        self.return_cartesian = return_cartesian
-
-        self.tg = trajectory_generator(
-            environment=environment, seq_len=seq_len, **kwargs
-        )
-
-    def set_start_pose(self, angle0=None, p0=None, **kwargs):
-        """
-        Set a fixed start POSE for the generator - used for rate map calculations
-        s.t agent is placed deterministically within every area of an arena.
-        """
-        self.tg = trajectory_generator(
-            environment=self.environment,
-            seq_len=self.seq_len,
-            angle0=angle0,
-            p0=p0,
-            **kwargs,
-        )
+        self.dataset_size = dataset_size
+        self.kwargs = kwargs
 
     def __len__(self):
         """
-        Length of dataset. But since generated, set
-        to a large fixed value.
+        Define size of an epoch. Since dataset is generated this number
+        can be chosen arbitrarily.
         """
-        return self.nsteps * self.batch_size
+        return self.dataset_size
 
     def __getitem__(self, index):
-        """Note, 'index' is ignored. Assumes only getting single elements"""
-        pos, vel = next(self.tg)[:2]
-        pos = torch.tensor(pos, dtype=torch.float32)
-        vel = torch.tensor(vel, dtype=torch.float32)
-        pc_pos = self.place_cells.softmax_response(pos)
-        init_pos, labels = pc_pos[0], pc_pos[1:]
-        vel = vel[1:]  # first velocity is a dummy velocity
+        """
+        Get a training (input & label) sample from the dataset.
 
-        if self.return_cartesian:
-            # for prediction phase (NOT training)
-            return [[vel, init_pos], labels, pos]
-        return [[vel, init_pos], labels]
+        Note! 'index' is ignored since dataset is generated.
+
+        Note2! Other quantities (e.g. euclidean positions) can be
+        access by for example - all as np.arrays!:
+        dataset[0]
+        dataset.agent.positions
+        """
+        self.agent.reset()
+        for _ in range(self.seq_len):
+            self.agent.step()
+
+        # Cast positions (in pc-basis) and velocities to tf.tensors
+        velocities = torch.tensor(self.agent.velocities, dtype=torch.float32)[1:]
+        positions = torch.tensor(self.agent.positions, dtype=torch.float32)
+        pc_positions = self.place_cells.softmax_response(positions)
+        init_pc_positions, labels = pc_positions[0], pc_positions[:1]
+        return [[velocities, init_pc_positions], labels]
 
 
-def rate_map(
+def rate_maps(
     model,
-    environment,
     dataset,
-    seq_len,
+    num_trajectories=1000,
     res=np.array([20, 20]),
     idxs=slice(0, 64, 1),
-    num_samples=1,
-    *args,
     **kwargs,
 ):
-    dataset.return_cartesian = True
-    board = environment.get_board(res)
-    num_response_maps = (idxs.stop - idxs.start) // idxs.step
-    response_maps = np.zeros((num_response_maps, *res))
-    count_maps = np.zeros((num_response_maps, *res))
-    dres = (environment.boxsize - environment.origo) / res
-
-    # Calculate grid cell responses
-    for n in range(num_samples):
-        # sample 'same' positions multiple times at different head directions
-        angle0 = None  # None implies random sampled head direction
-        for i in range(board.shape[0]):
-            for j in range(board.shape[1]):
-                p0 = np.random.uniform(
-                    low=np.maximum(board[i, j] - dres / 2, environment.origo),
-                    high=np.minimum(board[i, j] + dres / 2, environment.boxsize),
-                )
-                p0 = p0[None]  # p0 needs shape=(1,2)
-                # reinitialise pytorch dataset generator
-                dataset.set_start_pose(angle0=angle0, p0=p0, **kwargs)
-                # sample data from generator
-                inputs, labels, pos = dataset[0]
-                pos = pos.numpy()
-                # assign activity to space (from environment coordinates to pixel-coordinates)
-                pos_idxs = np.around(
-                    (res - 1) * pos / np.array(environment.boxsize)
-                ).astype(int)
-                if model == "labels":
-                    model_cell_response = labels.numpy()[None]  # add empty-batch dim
-                else:
-                    model_cell_response = model(inputs).detach().cpu().numpy()
-                response_maps[
-                    :, pos_idxs[:-1, 0], pos_idxs[:-1, 1]
-                ] += model_cell_response[0, :, idxs].T
-                count_maps[:, pos_idxs[:-1, 0], pos_idxs[:-1, 1]] += 1
-
-    # reset dataset-object
-    dataset.set_start_pose(angle0=None, p0=None, **kwargs)
-    dataset.return_cartesian = False
-
-    rate_maps = response_maps / count_maps
-    return board, rate_maps, response_maps, count_maps
-
-
-def save_obj(obj, pickle_object_path):
-    """
-    taken from:
-    https://stackoverflow.com/questions/19201290/how-to-save-a-dictionary-to-a-file/32216025
-    https://stackoverflow.com/questions/12517451/automatically-creating-directories-with-file-output
-    """
-    import os
-    if not os.path.exists(os.path.dirname(pickle_object_path)):
-        os.makedirs(os.path.dirname(pickle_object_path))
-    with open(pickle_object_path + ".pkl", "wb") as f:
-        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
-
-
-def load_obj(pickle_object_path):
-    with open(pickle_object_path + ".pkl", "rb") as f:
-        return pickle.load(f)
+    activities, x, y = [], [], []
+    for _ in range(num_trajectories):
+        activities.append(model(dataset[0][0])[0, :, idxs].detach().cpu().numpy())
+        x.append(dataset.agent.positions[1:, 0])
+        y.append(dataset.agent.positions[1:, 1])
+    activities = np.array(activities)
+    return scipy.stats.binned_statistic_2d(
+        np.ravel(x),
+        np.ravel(y),
+        activities.reshape(-1, activities.shape[-1]).T,
+        bins=res,
+        **kwargs,
+    )
 
 
 def multicontourf(xx, yy, zz):
